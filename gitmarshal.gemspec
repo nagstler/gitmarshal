9ddbdbe0dcef31bcd8ec79f81cd73a2e57be0859
nagstler <nagendra.dhanakeerthi@gmail.com>
2023-06-28 14:15:12 +0530

    Modify today command to only print table if there are commits today

--- gitmarshal.gemspec
+++ gitmarshal.gemspec
@@ -8,17 +8,17 @@ Gem::Specification.new do |spec|
   spec.authors = ["Nagendra Dhanakeerthi"]
   spec.email = ["nagendra.dhanakeerthi@gmail.com"]
 
-  spec.summary = "TODO: Write a short summary, because RubyGems requires one."
-  spec.description = "TODO: Write a longer description or delete this line."
-  spec.homepage = "TODO: Put your gem's website or public repo URL here."
+  spec.summary = "Write a short summary, because RubyGems requires one."
+  spec.description = "Write a longer description or delete this line."
+  spec.homepage = "https://github.com/nagstler/gitmarshal"
   spec.license = "MIT"
   spec.required_ruby_version = ">= 2.6.0"
 
-  spec.metadata["allowed_push_host"] = "TODO: Set to your gem server 'https://example.com'"
+  spec.metadata["allowed_push_host"] = "https://github.com/nagstler/gitmarshal"
 
-  spec.metadata["homepage_uri"] = spec.homepage
-  spec.metadata["source_code_uri"] = "TODO: Put your gem's public repo URL here."
-  spec.metadata["changelog_uri"] = "TODO: Put your gem's CHANGELOG.md URL here."
+  spec.metadata["homepage_uri"] = "https://github.com/nagstler/gitmarshal"
+  spec.metadata["source_code_uri"] = "https://github.com/nagstler/gitmarshal"
+  # spec.metadata["changelog_uri"] = "TODO: Put your gem's CHANGELOG.md URL here."
 
   # Specify which files should be added to the gem when it is released.
   # The `git ls-files -z` loads the files in the RubyGem that have been added into git.
@@ -37,6 +37,7 @@ Gem::Specification.new do |spec|
   spec.add_dependency 'rest-client', '~> 2.1'
   spec.add_dependency 'dotenv', '~> 2.7'
   spec.add_dependency 'thor', '~> 1.1'
+  spec.add_dependency 'terminal-table', '~> 3.0.0'
 
 
   # For more information and examples about making a new gem, check out our
--- lib/gitmarshal/cli.rb
+++ lib/gitmarshal/cli.rb
@@ -3,6 +3,8 @@
 require 'thor'
 require 'json'
 require 'rest-client'
+require 'date'
+require 'terminal-table'
 
 module GitMarshal
   class CLI < Thor
@@ -10,11 +12,56 @@ module GitMarshal
 
     desc "today", "Prints a summary of the authenticated user's GitHub activities and contributions for the current day"
     def today
-      # Our code will go here
-      puts "Prints Github summary for today"
+      user = fetch_user
+      repos = fetch_repos(user["login"])
+
+      puts "GitHub Daily Activity Snapshot for #{user['login']}"
+      puts "Date: #{Date.today}"
+      
+      repos.each do |repo|
+        commits = fetch_commits_for_repo(user["login"], repo["name"])
+
+        today_commits = commits.select do |commit|
+          commit_author = commit['commit']['author']['name']
+          commit_date = Date.parse(commit['commit']['author']['date'])
+          commit_author == user['name'] && commit_date == Date.today
+        end
+
+        if today_commits.size > 0
+          latest_commit = commits.first
+
+          rows = [
+            ['Commits Today', today_commits.size],
+            ['Total Commits', commits.size],
+            ['Latest Commit Message', latest_commit['commit']['message']],
+            ['Latest Commit Date', Date.parse(latest_commit['commit']['author']['date'])]
+          ]
+
+          puts "\nRepository: #{repo['name']}"
+          table = Terminal::Table.new :rows => rows
+          puts table
+        end
+      end
     end
 
+    default_command :today
+
     private
+
+    def fetch_user
+      response = RestClient.get("#{GITHUB_API_BASE_URL}/user", headers)
+      JSON.parse(response.body)
+    end
+
+    def fetch_repos(username)
+      response = RestClient.get("#{GITHUB_API_BASE_URL}/users/#{username}/repos", headers)
+      JSON.parse(response.body)
+    end
+
+    def fetch_commits_for_repo(username, repo_name)
+      response = RestClient.get("#{GITHUB_API_BASE_URL}/repos/#{username}/#{repo_name}/commits", headers)
+      JSON.parse(response.body)
+    end
 
     def headers
       { Authorization: "Bearer #{ENV['GITHUB_TOKEN']}" }
