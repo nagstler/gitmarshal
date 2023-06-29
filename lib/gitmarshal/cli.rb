73277b83ab1d77f525de262f06025069972e3bf0
Nagendra Dhanakeerthi <nagendra.dhanakeerthi@gmail.com>
2023-06-29 21:17:11 +0530

    Refactor CLI command handling to make usage more intuitive
    - Overwrite Thor's method_missing to treat unknown methods as repo names.
    - Add check for invalid repository names.
    - Modify the help message to reflect the new command usage.
    - Enable passing only the repo-name as an argument, eliminating the need to pass the 'metrics' keyword.
    - Set repos as the default command, it is executed when no argument is given.

--- lib/gitmarshal/cli.rb
+++ lib/gitmarshal/cli.rb
@@ -1,80 +1,63 @@
 require 'thor'
-require 'date'
 require 'terminal-table'
 require 'colorize'
 require_relative 'github_fetcher'
 
 module GitMarshal
   class CLI < Thor
-    desc "today", "Prints a summary of the authenticated user's GitHub activities and contributions for the current day"
-    method_option :repo, aliases: "-r", desc: "Specify a single repository name"
-    def today
+    class_option :help, type: :boolean, aliases: '-h', desc: 'Display usage information'
+
+    desc "repos", "Prints a summary of the authenticated user's GitHub repositories"
+    def repos
       fetcher = GithubFetcher.new
       user = fetcher.fetch_user
-      repos = options[:repo] ? [fetcher.fetch_repo(user["login"], options[:repo])] : fetcher.fetch_repos(user["login"])
-      
-      puts "GitHub Daily Activity Snapshot for #{user['login']}".colorize(:blue).bold
-      puts "Date: #{Date.today}"
+      repos = fetcher.fetch_repos
 
-      repos.each do |repo|
-        process_repo(fetcher, user, repo) if repo
-      end
-    end
+      puts "GitHub Repositories for #{user}".colorize(:blue).bold
 
-    default_command :today
-
-    private
-
-    def process_repo(fetcher, user, repo)
-      commits = fetcher.fetch_commits_for_repo(user["login"], repo["name"])
-      languages = fetcher.fetch_languages_for_repo(user["login"], repo["name"]).keys.join(', ')
-      today_commits = fetcher.fetch_commits_for_today(user["login"], repo["name"])
-
-      if today_commits.size > 0
-        display_summary_of_the_day(fetcher, user, repo, commits, languages, today_commits)
-        display_detailed_repo_metrics(fetcher, user, repo, commits)
-      end
-    end
-
-    def display_summary_of_the_day(fetcher, user, repo, commits, languages, today_commits)
-      latest_commit = commits.first
-      pr_raised_today = fetcher.fetch_pull_requests_for_today(user["login"], repo["name"]).count
-      build_status = fetcher.fetch_build_status(user["login"], repo["name"])
-
-      rows = [
-        ['Commits Today', today_commits.size],
-        ['PRs Raised Today', pr_raised_today]
-      ]
-
-      puts "\nRepository: #{repo['name']}".colorize(:yellow).bold
-      puts "Languages:".colorize(:cyan)+" #{languages}"
-      puts "Latest Commit Message:".colorize(:cyan)
-      puts "#{latest_commit['commit']['message']}"
-      puts "Latest Commit Date:".colorize(:cyan)+" #{Date.parse(latest_commit['commit']['author']['date'])}"
-      puts "Build Status:".colorize(:cyan)+" #{build_status}"
-      table = Terminal::Table.new :title => "Repo Summary of the Day".colorize(:green), :headings => ['Item', 'Count'].map { |i| i.colorize(:magenta) }, :rows => rows
+      rows = repos.map { |repo| [repo['name'], repo['description']] }
+      table = Terminal::Table.new :title => "Repositories".colorize(:green), :headings => ['Name', 'Description'].map { |i| i.colorize(:magenta) }, :rows => rows
       table.style = { :border_x => "=", :border_i => "x", :alignment => :center }
       puts table
     end
 
-    def display_detailed_repo_metrics(fetcher, user, repo, commits)
-      stargazers = fetcher.fetch_stargazers_for_repo(user["login"], repo["name"]).count
-      forks = fetcher.fetch_forks_for_repo(user["login"], repo["name"]).count
-      total_contributors = fetcher.fetch_contributors_for_repo(user["login"], repo["name"]).count
-      open_prs = fetcher.fetch_pull_requests_for_repo(user["login"], repo["name"]).select { |pr| pr['state'] == 'open' }.count
-      open_issues = fetcher.fetch_issues_for_repo(user["login"], repo["name"]).select { |issue| issue['state'] == 'open' }.count
-
-      additional_rows = [
-        ['Total Commits', commits.size],
-        ['Stargazers', stargazers],
-        ['Forks', forks],
-        ['Total Contributors', total_contributors],
-        ['Open Pull Requests', open_prs],
-        ['Open Issues', open_issues]
-      ]
-      additional_table = Terminal::Table.new :title => "Detailed Repo Metrics".colorize(:green), :headings => ['Metric', 'Count'].map { |i| i.colorize(:magenta) }, :rows => additional_rows
-      additional_table.style = { :border_x => "=", :border_i => "x", :alignment => :center }
-      puts additional_table
+    def help(*)
+      puts "You can either call ./bin/gitmarshal to list all repositories or ./bin/gitmarshal repo-name to show the metrics of the given repository."
     end
+
+    private
+
+    def metrics(repo_name)
+      fetcher = GithubFetcher.new
+      user = fetcher.fetch_user
+
+      repo = fetcher.fetch_repo_metrics(user, repo_name)
+
+      puts "GitHub Repository Metrics for #{repo_name}".colorize(:blue).bold
+
+      rows = [
+        ['Description', repo['data']['repository']['description']],
+        ['Total Commits', repo['data']['repository']['ref']['target']['history']['edges'].count],
+        ['Pull Requests', repo['data']['repository']['pullRequests']['totalCount']],
+        ['Issues', repo['data']['repository']['issues']['totalCount']],
+        ['Stargazers', repo['data']['repository']['stargazers']['totalCount']],
+        ['Forks', repo['data']['repository']['forks']['totalCount']],
+        ['Languages', repo['data']['repository']['languages']['edges'].map{|lang| lang['node']['name']}.join(', ')]
+      ]
+
+      table = Terminal::Table.new :title => "Repository Metrics".colorize(:green), :headings => ['Metric', 'Value'].map { |i| i.colorize(:magenta) }, :rows => rows
+      table.style = { :border_x => "=", :border_i => "x", :alignment => :center }
+      puts table
+    end
+
+    def method_missing(method, *_args, &_block)
+      if method =~ /[-a-zA-Z0-9_.]+/
+        metrics(method.to_s)
+      else
+        super
+      end
+    end
+
+    default_command :repos
   end
 end
--- lib/gitmarshal/github_fetcher.rb
+++ lib/gitmarshal/github_fetcher.rb
@@ -1,92 +1,100 @@
 require 'json'
-require 'rest-client'
-require 'date'
+require 'net/http'
 
 class GithubFetcher
-  GITHUB_API_BASE_URL = "https://api.github.com"
+  GITHUB_API_BASE_URL = "https://api.github.com/graphql"
+
+  def fetch_repos
+    query = <<~GRAPHQL
+      query {
+        viewer {
+          repositories(first: 100) {
+            nodes {
+              name
+              description
+            }
+          }
+        }
+      }
+    GRAPHQL
+  
+    response = execute_graphql_query(query)
+    JSON.parse(response.body)["data"]["viewer"]["repositories"]["nodes"]
+  end
+  
+
+  def fetch_repo_metrics(owner, repo_name)
+    query = <<~GRAPHQL
+      query {
+        repository(owner: "#{owner}", name: "#{repo_name}") {
+          name
+          description
+          pullRequests {
+            totalCount
+          }
+          issues {
+            totalCount
+          }
+          stargazers {
+            totalCount
+          }
+          forks {
+            totalCount
+          }
+          languages(first: 100) {
+            edges {
+              node {
+                name
+              }
+            }
+          }
+          ref(qualifiedName: "main") {
+            target {
+              ... on Commit {
+                history(first: 100) {
+                  edges {
+                    node {
+                      committedDate
+                      message
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    GRAPHQL
+
+    response = execute_graphql_query(query)
+    JSON.parse(response.body)
+  end
 
   def fetch_user
-    response = RestClient.get("#{GITHUB_API_BASE_URL}/user", headers)
-    JSON.parse(response.body)
+    query = <<~GRAPHQL
+      query {
+        viewer {
+          login
+        }
+      }
+    GRAPHQL
+  
+    response = execute_graphql_query(query)
+    JSON.parse(response.body)["data"]["viewer"]["login"]
   end
-
-  def fetch_repo(username, repo_name)
-    response = RestClient.get("#{GITHUB_API_BASE_URL}/repos/#{username}/#{repo_name}", headers)
-    JSON.parse(response.body)
-  end
-
-  def fetch_repos(username)
-    response = RestClient.get("#{GITHUB_API_BASE_URL}/users/#{username}/repos", headers)
-    JSON.parse(response.body)
-  end
-
-  def fetch_commits_for_repo(username, repo_name)
-    response = RestClient.get("#{GITHUB_API_BASE_URL}/repos/#{username}/#{repo_name}/commits", headers)
-    JSON.parse(response.body)
-  end
-
-  def fetch_commits_for_today(username, repo_name)
-    commits = fetch_commits_for_repo(username, repo_name)
-    commits.select do |commit|
-      commit_author = commit['commit']['author']['name']
-      commit_date = Date.parse(commit['commit']['author']['date'])
-      commit_author == fetch_user['name'] && commit_date == Date.today
-    end
-  end
-
-  def fetch_pull_requests_for_repo(username, repo_name)
-    response = RestClient.get("#{GITHUB_API_BASE_URL}/repos/#{username}/#{repo_name}/pulls", headers)
-    JSON.parse(response.body)
-  end
-
-  def fetch_pull_requests_for_today(username, repo_name)
-    response = RestClient.get("#{GITHUB_API_BASE_URL}/repos/#{username}/#{repo_name}/pulls?state=all", headers)
-    pull_requests = JSON.parse(response.body)
-    
-    today_pull_requests = pull_requests.select do |pr|
-      pr_date = Date.parse(pr['created_at'])
-      pr_date == Date.today
-    end
-    
-    today_pull_requests
-  end
-
-  def fetch_issues_for_repo(username, repo_name)
-    response = RestClient.get("#{GITHUB_API_BASE_URL}/repos/#{username}/#{repo_name}/issues", headers)
-    JSON.parse(response.body)
-  end
-
-  def fetch_stargazers_for_repo(username, repo_name)
-    response = RestClient.get("#{GITHUB_API_BASE_URL}/repos/#{username}/#{repo_name}/stargazers", headers)
-    JSON.parse(response.body)
-  end
-
-  def fetch_forks_for_repo(username, repo_name)
-    response = RestClient.get("#{GITHUB_API_BASE_URL}/repos/#{username}/#{repo_name}/forks", headers)
-    JSON.parse(response.body)
-  end
-
-  def fetch_languages_for_repo(username, repo_name)
-    response = RestClient.get("#{GITHUB_API_BASE_URL}/repos/#{username}/#{repo_name}/languages", headers)
-    JSON.parse(response.body)
-  end
-
-  def fetch_contributors_for_repo(username, repo_name)
-    response = RestClient.get("#{GITHUB_API_BASE_URL}/repos/#{username}/#{repo_name}/contributors", headers)
-    JSON.parse(response.body)
-  end
-
-  def fetch_build_status(username, repo_name)
-    response = RestClient.get("#{GITHUB_API_BASE_URL}/repos/#{username}/#{repo_name}/actions/runs", headers)
-    workflow_runs = JSON.parse(response.body)['workflow_runs']
-    
-    latest_run = workflow_runs.first
-    latest_run ? latest_run['conclusion'] : 'No workflow runs found'
-  end
+  
 
   private
-  
-  def headers
-    { Authorization: "Bearer #{ENV['GITHUB_TOKEN']}" }
+
+  def execute_graphql_query(query)
+    uri = URI(GITHUB_API_BASE_URL)
+    req = Net::HTTP::Post.new(uri, 
+      "Content-Type" => "application/json", 
+      "Authorization" => "Bearer #{ENV['GITHUB_TOKEN']}"
+    )
+    req.body = {query: query}.to_json
+    Net::HTTP.start(uri.hostname, uri.port, :use_ssl => uri.scheme == 'https') do |http|
+      http.request(req)
+    end
   end
 end
